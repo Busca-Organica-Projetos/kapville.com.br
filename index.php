978bd0f415f2e24f61cb1fadfe9fe22f2ad3b4b6
JoaoSabinoBuorg <joao.sabino@buscaorganica.com.br>
2026-05-28 15:41:23 -0300

    correção de html v1

--- index.php
+++ index.php
@@ -344,7 +344,7 @@ $borg->cssCompress(array(
                                 </button>
                                 <button class="next" type="button" aria-label="Próximo">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 320 512" aria-hidden="true">
-                                        <path fill="currentColor" d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z">
+                                        <path fill="currentColor" d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
                                     </svg>
                                 </button>
                             </div>
