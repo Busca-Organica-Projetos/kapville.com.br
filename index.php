1ad3d03624c097fd09fe382a53479d9d37d91194
JoaoSabinoBuorg <joao.sabino@buscaorganica.com.br>
2026-05-29 11:08:44 -0300

    correção carrossel avaliações

--- index.php
+++ index.php
@@ -602,16 +602,16 @@ $borg->cssCompress(array(
                             </div>
                         </div>
 
-                        <a class="prev" data-click-track="Avaliação Anterior - Avaliações - Home">
+                        <button class="prev" type="button" data-click-track="Avaliação Anterior - Avaliações - Home" aria-label="Avaliação anterior">
                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 320 512" aria-hidden="true">
                                 <path fill="currentColor" d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/>
                             </svg>
-                        </a>
-                        <a class="next" data-click-track="Próxima Avaliação - Avaliações - Home">
+                        </button>
+                        <button class="next" type="button" data-click-track="Próxima Avaliação - Avaliações - Home" aria-label="Próxima avaliação">
                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 320 512" aria-hidden="true">
                                 <path fill="currentColor" d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/>
                             </svg>
-                        </a>
+                        </button>
 
                     </div>
                 </div>
